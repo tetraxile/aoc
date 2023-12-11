d7c9a9cf4e260b188355275c324f35a286537cd7
tetraxile <tetraxile@gmail.com>
2023-12-11 11:46:20 +0000

    2023 day 5 part 2

--- 2023/5.py
+++ 2023/5.py
@@ -22,21 +22,46 @@ def part_1(data: str) -> str:
 
 def part_2(data: str) -> str:
     sections = data.split("\n\n")
-    seeds = ints(sections[0].split(":")[1])
-    seeds = [seeds[i:i+2] for i in range(0, len(seeds), 2)]
-    
-    # good luck having enough ram + patience to do this
-    seeds = [seed for pair in seeds for seed in range(pair[0], sum(pair))]
+    tmp = ints(sections[0].split(":")[1])
+    ranges = [range(pair[0], sum(pair)) for pair in chunks(tmp, 2)]
 
-    for section in sections[1:]:
+    for j, section in enumerate(sections[1:]):
         lines = section.split("\n")
         src_name, _, dst_name = lines[0].removesuffix(" map:").split("-")
-        lines = list(map(ints, lines[1:]))
+        lines = [(range(src_start, src_start+length), range(dst_start, dst_start+length)) for dst_start, src_start, length in sorted(map(ints, lines[1:]), key=lambda k: k[1])]
 
-        for i, seed in enumerate(seeds):
-            for dst_start, src_start, length in lines:
-                if src_start <= seed < src_start + length:
-                    seeds[i] = dst_start + (seed - src_start)
+        new_ranges = []
+        for i, rng in enumerate(ranges):
+            entered_src_range = None
+            left_src_range = False
+
+            for src_range, dst_range in lines:
+                difference = dst_range.start - src_range.start
+
+                if rng.start in src_range:
+                    entered_src_range = rng.start
+                if (rng.stop - 1) in src_range or src_range.start >= rng.stop:
+                    left_src_range = True
+
+                if entered_src_range is None:
+                    continue
+
+                if left_src_range:
+                    entered_src_range = max(entered_src_range, src_range.start)
+                    new_ranges.append(range(entered_src_range + difference, rng.stop + difference))
                     break
+                else:
+                    new_ranges.append(range(max(rng.start + difference, dst_range.start), min(rng.stop + difference, dst_range.stop)))
 
-    return min(seeds)
+            if entered_src_range is None:
+                new_ranges.append(rng)
+            elif not left_src_range:
+                new_ranges.append(range(src_range.stop, rng.stop))
+
+        ranges = new_ranges
+
+    min_location = float("inf")
+    for rng in ranges:
+        min_location = min(rng.start, min_location)
+
+    return min_location
